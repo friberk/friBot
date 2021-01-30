372a27e516a93bd7ee09d0e7d1df0bb13d3a2e6d
Berk Çakar <berk2238@hotmail.com>
2021-01-30 21:57:05 +0300

    Update FriBotMsgEvents.java
    updated count filter system

--- src/main/java/com/fribot/FriBotMsgEvents.java
+++ src/main/java/com/fribot/FriBotMsgEvents.java
@@ -11,7 +11,6 @@ public class FriBotMsgEvents
     // Properties
 
     int keremCounter = 0;
-    static final String KEREM_ID = "198374148852940800";
     
     // Constructors
     
@@ -34,10 +33,13 @@ public class FriBotMsgEvents
         .map(MessageCreateEvent::getMessage)
         .filter(message -> message.getAuthor().map(user -> user.getId()
                                                                .asString()
-                                                               .equals(KEREM_ID)).orElse(false)) // isBot check is redundant since the ID is known
+                                                               .equals( "198374148852940800")).orElse(false)) // isBot check is redundant since the ID is known
         .filter(message -> {
-            keremCounter++;
+            this.keremCounter++;
             return true;
+        })
+        .filter(message -> {
+            return this.keremCounter % 5 == 0;
         }) // Stupid "filter"
         .flatMap(Message::getChannel)
         .flatMap(channel -> channel.createMessage("test"))
